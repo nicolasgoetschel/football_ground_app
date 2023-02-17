245776856982af4d4d3cccdd416a5e568ad6890f
Nicolas Goetschel <nicgoe@hotmail.com>
2023-02-17 14:41:41 +0000

    football_ground class

---
+++ models/ground.py
@@ -0,0 +1,7 @@
+class FootballGround:
+    def __init__(self, name, team, location, capacity, visited=False):
+        self.name = name
+        self.team = team
+        self.location = location
+        self.capacity = capacity
+        self.visited = visited
