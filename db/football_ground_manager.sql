fcdc48606e31c9412ba987fe08b4fa7b7f5f0b74
Nicolas Goetschel <nicgoe@hotmail.com>
2023-02-18 10:15:34 +0000

    countries table

--- db/football_ground_manager.sql
+++ db/football_ground_manager.sql
@@ -2,3 +2,9 @@ DROP TABLE IF EXISTS countries;
 DROP TABLE IF EXISTS football_grounds;
 DROP TABLE IF EXISTS leagues;
 
+CREATE TABLE countries (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),   
+    flag VARCHAR(255), 
+    league VARCHAR(255)
+);
