908093c6e592e4e487538f74a000ce02a4687b95
Nicolas Goetschel <nicgoe@hotmail.com>
2023-02-18 10:46:57 +0000

    seeds.py created

---
+++ seeds.py
@@ -0,0 +1,10 @@
+import pdb
+from models.country import Country
+from models.league import League
+from models.football_ground import FootballGround
+
+import repositories.country_repository as country_repository
+import repositories.league_repository as league_repository
+import repositories.football_ground_repository as football_ground_repository
+
+pdb.set_trace()
