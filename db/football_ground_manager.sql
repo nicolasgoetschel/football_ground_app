c75d0b74181408398ecd3e8623cfb54fd5cc6441
Nicolas Goetschel <nicgoe@hotmail.com>
2023-02-18 10:19:24 +0000

    tables created

--- db/football_ground_manager.sql
+++ db/football_ground_manager.sql
@@ -8,3 +8,19 @@ CREATE TABLE countries (
     flag VARCHAR(255), 
     league VARCHAR(255)
 );
+
+CREATE TABLE leagues (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),   
+    logo VARCHAR(255), 
+    football_ground VARCHAR(255)
+);
+
+CREATE TABLE football_grounds (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),   
+    team VARCHAR(255), 
+    location VARCHAR(255),
+    capacity VARCHAR(255),
+    visited BOOLEAN
+);
